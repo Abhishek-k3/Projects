3c68170ec43c1570f3cabfe477c73c04e42574f3
Abhishek-k3 <69759663+Abhishek-k3@users.noreply.github.com>
2021-05-21 10:47:22 +0530

    Update BillPayer[Jio].py

--- BillPayment/BillPayer[Jio].py
+++ BillPayment/BillPayer[Jio].py
@@ -7,14 +7,20 @@ from selenium.webdriver.common.keys import Keys
 
 
 driver = webdriver.Chrome()
+print("----------------Automatic modile bill payment-------------")
+print("--------------------Project By Abhishek.K-----------------")
+print("--------------e-mail me at: abhisheku3u@gmail.com---------")
+print("-----------------Contact me for publishing----------------")
 
 # url
 driver.get('https://www.jio.com/selfcare/recharge/mobility/')
 
 phoneNo = driver.find_element_by_name("jioNrInputName")
-phoneNo.send_keys("8296604013")
+phoneNo.send_keys("8296604013")   #Change the number here if needed
 phoneNo.send_keys(Keys.RETURN)
 driver.get("https://www.stackoverflow.com")
 
 a = driver.find_element_by_xpath("//*[@id='__next']/div/div[2]/div[1]/div[2]/div[2]/div[2]/div[10]/div[2]/div[2]/button[2]")
 a.click()
+driver.close()
+#The project is bounded till here as the website is unstable and had a lot of timeout problems
